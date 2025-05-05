bdb62ee569c60f52796b0b1c9c5dd092531b31a9
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-05-05 11:25:45 +0200

    Create the package provider

---
+++ src/Providers/CheckoutServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CheckoutServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void {}
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void {}
+}
