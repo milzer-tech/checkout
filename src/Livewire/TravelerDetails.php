9389c36ae2363ded8038a25c9c22e8ee4cc7eeef
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-05-06 08:56:10 +0200

    Update properties

--- src/Livewire/TravelerDetails.php
+++ src/Livewire/TravelerDetails.php
@@ -6,6 +6,7 @@ namespace Nezasa\Checkout\Livewire;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 
 class TravelerDetails extends Component
@@ -13,34 +14,32 @@ class TravelerDetails extends Component
     /**
      * The unique identifier for the checkout process.
      */
+    #[Url]
     public string $checkoutId;
 
     /**
      * The unique identifier for the itinerary
      */
+    #[Url]
     public string $itineraryId;
 
     /**
      * Indicates the request's source from the IBE or the APP.
      * This can help determine if the user is authenticated (APP) or not (IBE).
      */
+    #[Url]
     public string $origin;
 
     /**
      * The ISO 639-1 language code representing the user's language preference for the itinerary.
      */
+    #[Url]
     public string $lang;
 
     /**
      * Mount the component with the request data.
      */
-    public function mount(Request $request): void
-    {
-        $this->checkoutId = $request->str('checkoutId')->toString();
-        $this->itineraryId = $request->str('itineraryId')->toString();
-        $this->origin = $request->str('origin')->toString();
-        $this->lang = $request->str('lang')->toString();
-    }
+    public function mount(Request $request): void {}
 
     /**
      * Render the component view.
