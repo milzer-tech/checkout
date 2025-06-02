3e38cf65e6c304eb86fde89d756c0cee669cea55
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-06-02 16:41:36 +0200

    Add connecitons

--- src/Integrations/Nezasa/Dtos/Responses/Entites/LegResponseEntity.php
+++ src/Integrations/Nezasa/Dtos/Responses/Entites/LegResponseEntity.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\Entites;
 
+use Illuminate\Support\Collection;
 use Spatie\LaravelData\Data;
 
 class LegResponseEntity extends Data
@@ -11,11 +12,14 @@ class LegResponseEntity extends Data
     /**
      * Create a new instance of the LegResponseEntityDto
      *
+     * @param  Collection<int, LegConnectionEntity>  $connections
+     *
      * @link https://support.nezasa.com/hc/en-gb/articles/4404075693969-Planner-API
      *
      * @note There are other properties in this entity, but we don't need them for now.
      */
     public function __construct(
         public StopResponseEntity $stop,
+        public Collection $connections,
     ) {}
 }
