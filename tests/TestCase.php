ae4945d26d68aec9cedaf3fa846479ab2247dc3f
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-06-02 16:39:12 +0200

    Update Testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,18 @@
 
 namespace Tests;
 
+use Illuminate\Support\Carbon;
 use Nezasa\Checkout\Providers\CheckoutServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Saloon\Http\Faking\MockClient;
 use Saloon\MockConfig;
 use Spatie\LaravelData\LaravelDataServiceProvider;
+use Spatie\Snapshots\MatchesSnapshots;
 
 abstract class TestCase extends OrchestraTestCase
 {
+    use MatchesSnapshots;
+
     /**
      * Get package providers.
      *
@@ -37,11 +41,39 @@ abstract class TestCase extends OrchestraTestCase
 
         MockClient::destroyGlobal();
 
-        MockConfig::setFixturePath(__DIR__.'/'.str(static::class)
+        MockConfig::setFixturePath(__DIR__.'/'.str(get_class($this))
             ->after('P\Tests\\')
             ->replace('\\', '/')
             ->beforeLast('/')
             ->append('/saloon_responses/')
+        );
+    }
+
+    /**
+     * Get the snapshot directory for the current test class.
+     */
+    protected function getSnapshotDirectory(): string
+    {
+        return __DIR__.'/'.str(get_class($this))
+            ->after('P\Tests\\')
+            ->replace('\\', '/')
+            ->beforeLast('/')
+            ->append('/__snapshots__');
+    }
+
+    /**
+     * Set a fake Carbon datetime for testing purposes.
+     */
+    protected function fakeCarbon(
+        int $year = 2025,
+        int $month = 5,
+        int $day = 26,
+        int $hour = 11,
+        int $minute = 20,
+        int $second = 19
+    ): void {
+        Carbon::setTestNow(
+            Carbon::create($year, $month, $day, $hour, $minute, $second)
         );
     }
 }
