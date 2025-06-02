3aed2f3e53a9e85ea381c7bab8b22aa6666af5ff
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-06-02 10:57:59 +0200

    Configure set up

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,8 @@ namespace Tests;
 
 use Nezasa\Checkout\Providers\CheckoutServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Saloon\Http\Faking\MockClient;
+use Saloon\MockConfig;
 use Spatie\LaravelData\LaravelDataServiceProvider;
 
 abstract class TestCase extends OrchestraTestCase
@@ -11,16 +13,35 @@ abstract class TestCase extends OrchestraTestCase
     /**
      * Get package providers.
      *
-     * @api
-     *
      * @param  \Illuminate\Foundation\Application  $app
      * @return array<int, class-string>
+     *
+     * @api
      */
+    #[\Override]
     protected function getPackageProviders($app)
     {
         return [
             LaravelDataServiceProvider::class,
             CheckoutServiceProvider::class,
         ];
+    }
+
+    /**
+     * Clean up the testing environment before the next test.
+     */
+    #[\Override]
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        MockClient::destroyGlobal();
+
+        MockConfig::setFixturePath(__DIR__.'/'.str(static::class)
+            ->after('P\Tests\\')
+            ->replace('\\', '/')
+            ->beforeLast('/')
+            ->append('/saloon_responses/')
+        );
     }
 }
