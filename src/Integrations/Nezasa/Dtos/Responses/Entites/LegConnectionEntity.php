c42b3bc8bc5eb093ba32889cc19d81a3325cc546
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-06-02 16:40:33 +0200

    Create LegConnectionEntity

---
+++ src/Integrations/Nezasa/Dtos/Responses/Entites/LegConnectionEntity.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\Entites;
+
+use Carbon\CarbonImmutable;
+use Spatie\LaravelData\Data;
+
+class LegConnectionEntity extends Data
+{
+    /**
+     * Create a new instance of the LegConnectionEntity
+     */
+    public function __construct(
+        public string $name,
+        public string $connectionType,
+        public LocationResponseEntity $startLocation,
+        public LocationResponseEntity $endLocation,
+        public CarbonImmutable $startDateTime,
+        public CarbonImmutable $endDateTime,
+    ) {}
+}
