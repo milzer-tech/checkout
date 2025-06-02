05a46efee8acc70b600397f06515be38f4943931
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-06-02 16:40:10 +0200

    Create ItinerarySummary

---
+++ src/Dtos/Planner/ItinerarySummary.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Dtos\Planner;
+
+use Illuminate\Support\Collection;
+use Nezasa\Checkout\Dtos\Planner\Entities\ItineraryActivity;
+use Nezasa\Checkout\Dtos\Planner\Entities\ItineraryFlight;
+use Nezasa\Checkout\Dtos\Planner\Entities\ItineraryRentalCar;
+use Nezasa\Checkout\Dtos\Planner\Entities\ItineraryStay;
+use Nezasa\Checkout\Dtos\Planner\Entities\ItineraryTransfer;
+use Spatie\LaravelData\Data;
+
+class ItinerarySummary extends Data
+{
+    /**
+     * Create a new instance of the ItinerarySummary.
+     *
+     * @param  Collection<int, ItineraryStay>  $stays
+     * @param  Collection<int, ItineraryFlight>  $flights
+     * @param  Collection<int, ItineraryTransfer>  $transfers
+     * @param  Collection<int, ItineraryActivity>  $activities
+     * @param  Collection<int, ItineraryRentalCar>  $rentalCars
+     */
+    public function __construct(
+        public Collection $stays = new Collection,
+        public Collection $flights = new Collection,
+        public Collection $transfers = new Collection,
+        public Collection $activities = new Collection,
+        public Collection $rentalCars = new Collection,
+    ) {}
+}
