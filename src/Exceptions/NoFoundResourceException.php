d771ba044400b005dee6801374209c7fd09cb9c2
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-02 14:27:38 +0200

    Add NoFoundResourceException for handling not found errors

---
+++ src/Exceptions/NoFoundResourceException.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Exceptions;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+
+class NoFoundResourceException extends Exception
+{
+    /**
+     * Create a new instance of the ParentException
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            message: 'The requested resource could not be found.',
+            code: Response::HTTP_NOT_FOUND,
+        );
+    }
+}
