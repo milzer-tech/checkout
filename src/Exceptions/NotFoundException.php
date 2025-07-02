d6b37a0bfdb19333f8ab76cde5e5d6b1b522aeab
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-02 15:21:16 +0200

    Refactor NotFoundException to extend HttpException and set appropriate status code

--- src/Exceptions/NotFoundException.php
+++ src/Exceptions/NotFoundException.php
@@ -5,16 +5,19 @@ declare(strict_types=1);
 namespace Nezasa\Checkout\Exceptions;
 
 use Illuminate\Http\Response as LaravelResponse;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class NotFoundException extends NotFoundHttpException
+class NotFoundException extends HttpException
 {
     /**
      * Create a new instance of the ParentException
      */
     public function __construct()
     {
-        parent::__construct(message: 'The requested resource could not be retrieved from Nezasa API.');
+        parent::__construct(
+            statusCode: SymfonyResponse::HTTP_NOT_FOUND,
+            message: 'The requested resource could not be retrieved from Nezasa API.');
     }
 
     /**
@@ -24,7 +27,7 @@ class NotFoundException extends NotFoundHttpException
     {
         return new LaravelResponse(
             content: view(view: 'checkout::exceptions.all')->with('exception', $this),
-            status: $this->getStatusCode()
+            status: SymfonyResponse::HTTP_NOT_FOUND
         );
     }
 }
