1d3ce35000453e275e6eacd1775fb1e0afe7c3b3
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-04 13:38:44 +0200

    Add paxDetails property to GetItineraryResponse and implement methods for counting adults and children

---
+++ src/Integrations/Nezasa/Dtos/Responses/Entites/PaxDetailEntity.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\Entites;
+
+use Spatie\LaravelData\Data;
+
+class PaxDetailEntity extends Data
+{
+    /**
+     * Create a new instance of the PaxDetailEntity
+     *
+     * @link https://support.nezasa.com/hc/en-gb/articles/4404075693969-Planner-API
+     *
+     * @note There are other properties in this entity, but we don't need them for now.
+     */
+    public function __construct(
+        public string $id,
+        public ?int $age = null,
+    ) {}
+}
--- src/Integrations/Nezasa/Dtos/Responses/GetItineraryResponse.php
+++ src/Integrations/Nezasa/Dtos/Responses/GetItineraryResponse.php
@@ -8,6 +8,7 @@ use Carbon\CarbonImmutable;
 use Illuminate\Support\Collection;
 use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\Entites\LegConnectionEntity;
 use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\Entites\ModulesResponseEntity;
+use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\Entites\PaxDetailEntity;
 use Spatie\LaravelData\Data;
 
 class GetItineraryResponse extends Data
@@ -18,6 +19,7 @@ class GetItineraryResponse extends Data
      * @param  Collection<int, ModulesResponseEntity>  $modules
      * @param  Collection<int, LegConnectionEntity>  $startConnections
      * @param  Collection<int, LegConnectionEntity>  $returnConnections
+     * @param  Collection<int, PaxDetailEntity>  $paxDetails
      *
      * @link https://support.nezasa.com/hc/en-gb/articles/4404075693969-Planner-API
      *
@@ -27,7 +29,38 @@ class GetItineraryResponse extends Data
         public CarbonImmutable $startDate,
         public CarbonImmutable $endDate,
         public Collection $modules = new Collection,
+        public Collection $paxDetails = new Collection,
         public Collection $startConnections = new Collection,
         public Collection $returnConnections = new Collection,
     ) {}
+
+    /**
+     * Count the number of adults in the itinerary.
+     */
+    public function countAdults(): int
+    {
+        return $this->paxDetails->filter(fn (PaxDetailEntity $detail) => $detail->age >= 18)->count();
+    }
+
+    /**
+     * Count the number of adults in the itinerary.
+     */
+    public function countChildren(): int
+    {
+        return $this->paxDetails->filter(fn (PaxDetailEntity $detail) => $detail->age < 18)->count();
+    }
+
+    /**
+     * Get the ages of the children in the itinerary.
+     *
+     * @return Collection<int, int>
+     */
+    public function getChildrenAges(): Collection
+    {
+        return $this->paxDetails
+            ->filter(fn (PaxDetailEntity $detail) => $detail->age < 18)
+            ->pluck('age')
+            ->filter()
+            ->values();
+    }
 }
