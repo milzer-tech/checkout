a069bc88dbd4ab27583a44dac774a3051cee336f
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-11 16:44:34 +0200

    Add input component for dynamic field rendering and validation

---
+++ src/Resources/Views/components/input.blade.php
@@ -0,0 +1,13 @@
+<div class="space-y-2 w-full min-w-0">
+    <label class="block text-gray-700 dark:text-gray-200 font-medium overflow-ellipsis whitespace-nowrap overflow-hidden">
+        {{trans("checkout::input.attributes.$name")}}
+    </label>
+     <input type=@switch($label)
+     @case('email')
+      "email"
+      @break
+      @default
+      "text"
+      @endswitch wire:model="{{$wireModel}}" class="form-input w-full" placeholder="{{trans("checkout::input.placeholders.$name")}}" />
+      @error($wireModel)<span class="text-red-500 text-sm">{{ $message }}</span>@enderror
+</div>
