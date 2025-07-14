ca956057d2d466159abd5af0faca3aa52ed5f039
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-14 15:05:45 +0200

    Make 'checkout_id' unique in checkouts table migration

--- database/migrations/create_checkout_table.php
+++ database/migrations/create_checkout_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('checkouts', function (Blueprint $table) {
             $table->id();
-            $table->string('checkout_id')->index();
+            $table->string('checkout_id')->unique();
             $table->json('data')->nullable();
             $table->timestamps();
         });
