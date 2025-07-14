fb0b72fb105fff779a55ff97dfa6e6c0e3644c8d
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-14 09:48:00 +0200

    Refactor gender selection to use GenderEnum for dynamic options

--- src/Resources/Views/components/gender.blade.php
+++ src/Resources/Views/components/gender.blade.php
@@ -1,11 +1,14 @@
+@php use Nezasa\Checkout\Integrations\Nezasa\Enums\GenderEnum; @endphp
 <div class="space-y-2 w-full min-w-0">
-    <label class="block text-gray-700 dark:text-gray-200 font-medium overflow-ellipsis whitespace-nowrap overflow-hidden">
+    <label
+        class="block text-gray-700 dark:text-gray-200 font-medium overflow-ellipsis whitespace-nowrap overflow-hidden">
         {{trans('checkout::input.attributes.gender')}}
     </label>
     <select wire:model="{{$wireModel}}" class="form-select pr-8 w-full">
         <option>Select</option>
-        <option value="M">Male</option>
-        <option value="F">Female</option>
+        @foreach(GenderEnum::getLabels() as $value => $lable)
+            <option value="{{$value}}">{{$lable}}</option>
+        @endforeach
     </select>
-      @error($wireModel)<span class="text-red-500 text-sm">{{ $message }}</span>@enderror
+    @error($wireModel)<span class="text-red-500 text-sm">{{ $message }}</span>@enderror
 </div>
