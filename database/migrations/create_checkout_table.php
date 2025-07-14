53d5d1e80b02bb63ac33c38ab16200bcd36fb17f
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-14 12:32:20 +0200

    Add index to checkout_id in checkouts table migration

--- database/migrations/create_checkout_table.php
+++ database/migrations/create_checkout_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('checkouts', function (Blueprint $table) {
             $table->id();
-            $table->string('checkout_id');
+            $table->string('checkout_id')->index();
             $table->json('data');
             $table->timestamps();
         });
