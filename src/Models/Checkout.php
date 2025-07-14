c8de1e5e7015ab9822fac10ed4990acf2e5c1869
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-14 15:06:52 +0200

    Update Checkout model to use AsCollection for data casting and add updateData method

--- src/Models/Checkout.php
+++ src/Models/Checkout.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 
 namespace Nezasa\Checkout\Models;
 
-use Illuminate\Database\Eloquent\Casts\AsFluent;
+use Illuminate\Database\Eloquent\Casts\AsCollection;
 use Illuminate\Database\Eloquent\Model;
 
 class Checkout extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'checkout_id',
+        'data',
+    ];
+
     /**
      * Get the attributes that should be cast.
      *
@@ -17,7 +27,21 @@ class Checkout extends Model
     protected function casts(): array
     {
         return [
-            'data' => AsFluent::class,
+            'data' => AsCollection::class,
         ];
+    }
+
+    /**
+     * Update a specific key in the data collection;
+     */
+    public function updateData(string $kay, $value): bool
+    {
+        $array = $this->data?->toArray() ?? [];
+
+        $array = data_set($array, $kay, $value);
+
+        $this->data = $array;
+
+        return $this->save();
     }
 }
