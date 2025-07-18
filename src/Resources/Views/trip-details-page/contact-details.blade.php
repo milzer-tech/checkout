896a72a32d3b34283a9433b16352f7be2aa7fe77
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-18 09:04:30 +0200

    Refactor contact details form to use reusable input component

--- src/Resources/Views/trip-details-page/contact-details.blade.php
+++ src/Resources/Views/trip-details-page/contact-details.blade.php
@@ -5,75 +5,19 @@
 @endphp
 
 <x-checkout::editable-box
-    title="Contact details"
-    :state="$state"
-    :showEdit="$state === 'valid'"
-    :showCheck="$state === 'valid'"
-    onEdit="editContact"
+        title="Contact details"
+        :state="$state"
+        :showEdit="$state === 'valid'"
+        :showCheck="$state === 'valid'"
+        onEdit="editContact"
 >
     <form wire:submit="save" class="space-y-6">
-        @php($inputs = 0)
-        @php($openTag = false)
 
-        @foreach($contactRequirements as $name => $value)
-            @if($value->isHidden())
-                @continue
-            @endif
-
-            @if($inputs === 0 && !$openTag)
-                @php($openTag = true)
-                <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 min-w-0">
-                    @endif
-
-                    @if(! in_array($name, ['address1', 'address2', 'gender', 'mobilePhone']))
-                        @include('checkout::components.input', [
-                            'label' => $name,
-                            'wireModel' => "contact.$name",
-                            'placeholder' => $name,
-                        ])
-                        @php($inputs++)
-                    @endif
-
-                    @if($name === 'gender')
-                        @include('checkout::components.gender', ['wireModel' => "contact.$name"])
-                        @php($inputs++)
-                    @endif
-
-                    @if($name === 'mobilePhone')
-                        @include('checkout::components.phone', [
-                            'wireModel' => "contact.$name",
-                            'codes' => $countryCodes
-                           ])
-                        @php($inputs++)
-                    @endif
-
-                @if($inputs === 3 && $openTag)
-                    </div>
-                    @php($inputs = 0)
-                    @php($openTag = false)
-                @endif
-
-                @endforeach
-
-                @if($openTag)
-                        </div>
-                @endif
-
-
-                @unless($contactRequirements->address1->isHidden())
-                    <div class="grid grid-cols-2 lg:grid-cols-3 gap-6 min-w-0">
-                        @include('checkout::components.address', ['wireModel' => "contact.address1", 'name' => 'address1'])
-                    </div>
-                        @php($inputs++)
-                @endunless
-
-                @unless($contactRequirements->address2->isHidden())
-                    <div class="grid grid-cols-2 lg:grid-cols-3 gap-6 min-w-0">
-                        @include('checkout::components.address', ['wireModel' => "contact.address2", 'name' => 'address2'])
-                    </div>
-                    @php($inputs++)
-                @endunless
-
+        @include('checkout::trip-details-page.inputs',[
+            'requirements' => $contactRequirements,
+            'countryCodes' => $countryCodes,
+            'saveTo' => 'contact'
+])
 
             <div class="flex justify-end">
                 <button
