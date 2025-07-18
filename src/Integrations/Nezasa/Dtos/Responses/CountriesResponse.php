766117ecde77a5b9b24f35e2f8522ec59154947d
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-18 09:24:20 +0200

    Update CountriesResponse to rename parameter and add MapInputName attribute

--- src/Integrations/Nezasa/Dtos/Responses/CountriesResponse.php
+++ src/Integrations/Nezasa/Dtos/Responses/CountriesResponse.php
@@ -15,12 +15,9 @@ class CountriesResponse extends BaseDto
      *
      * @see https://docs.tripbuilder.app/Mo9reezaehiengah/location-api-v1.html#/paths/~1areas~1%7BrefId%7D~1pictures/get
      *
-     * @note There are other properties in the response, but we are only interested in the modules for now.
-     *
-     * @param  Collection<int, CountryResponseEntity>  $callingCodes
+     * @param  Collection<int, CountryResponseEntity>  $countries
      */
     public function __construct(
         public Collection $countries,
-
     ) {}
 }
