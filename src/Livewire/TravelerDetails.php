732a4c7bb16a6189ec799f72a8dc1db3f4b7159f
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-21 09:16:30 +0200

    Add validation rules for traveler details including gender and date of birth

--- src/Livewire/TravelerDetails.php
+++ src/Livewire/TravelerDetails.php
@@ -3,6 +3,7 @@
 namespace Nezasa\Checkout\Livewire;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Validation\Rules\Enum;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Nezasa\Checkout\Dtos\View\ShowTraveller;
@@ -10,6 +11,7 @@ use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\CountriesResponse;
 use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\CountryCodesResponse;
 use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\Entities\PassengerRequirementEntity;
 use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\Entities\PaxAllocationResponseEntity;
+use Nezasa\Checkout\Integrations\Nezasa\Enums\GenderEnum;
 use Nezasa\Checkout\Models\Checkout;
 
 class TravelerDetails extends Component
@@ -130,5 +132,63 @@ class TravelerDetails extends Component
         Checkout::query()
             ->firstOrCreate(['checkout_id' => $this->checkoutId])
             ->updateData($name, $value);
+    }
+
+    protected function rules(): array
+    {
+        $rules = [
+            'firstName' => ['string', 'max:255'],
+            'lastName' => ['string', 'max:255'],
+            'secondOrAdditionalName' => ['string', 'max:255'],
+            'passportNumber' => ['string', 'max:255'],
+            'nationality' => ['string'], // country response
+            'gender' => [new Enum(GenderEnum::class)],
+            'dateOfBirth' => ['array'],
+            'dateOfBirth.day' => ['integer', 'min:1', 'max:31'],
+            'dateOfBirth.month' => ['integer', 'min:1', 'max:12'],
+            'dateOfBirth.year' => ['integer', 'min:1900', 'max:'.date('Y')],
+            'passportExpirationDate' => ['array'],
+            'passportExpirationDate.day' => ['integer', 'min:1', 'max:31'],
+            'passportExpirationDate.month' => ['integer', 'min:1', 'max:12'],
+            'passportExpirationDate.year' => ['integer', 'min:1900', 'max:'.date('Y')],
+            'passportIssuingCountry' => ['string'], // country response
+            'postalCode' => ['string', 'max:20'],
+            'city' => ['string', 'max:255'],
+            'country' => ['string', 'max:255'],
+            'address1' => ['array'],
+            'address1.country' => ['string', 'max:255'],
+            'address1.countryCode' => ['string', 'max:10'],
+            'address1.city' => ['string', 'max:255'],
+            'address1.postalCode' => ['string', 'max:20'],
+            'address1.street1' => ['string', 'max:255'],
+            'address1.street2' => ['string', 'max:255'],
+            'address1.region' => ['string', 'max:255'],
+            'address2' => ['array'],
+            'address2.country' => ['string', 'max:255'],
+            'address2.countryCode' => ['string', 'max:10'],
+            'address2.city' => ['string', 'max:255'],
+            'address2.postalCode' => ['string', 'max:20'],
+            'address2.street1' => ['string', 'max:255'],
+            'address2.street2' => ['string', 'max:255'],
+            'address2.region' => ['string', 'max:255'],
+        ];
+
+        foreach ($this->passengerRequirements as $name => $item) {
+            $required = $item->isRequired() ? ['required'] : ['nullable'];
+
+            $rules[$name] = array_merge($required, $rules[$name]);
+
+            if ($name === 'address1' || $name === 'address2') {
+                $rules["$name.country"] = array_merge($required, $rules["$name.country"]);
+                $rules["$name.city"] = array_merge($required, $rules["$name.city"]);
+                $rules["$name.postalCode"] = array_merge($required, $rules["$name.postalCode"]);
+                $rules["$name.street1"] = array_merge($required, $rules["$name.street1"]);
+            }
+        }
+
+        return array_combine(
+            array_map(fn ($key) => 'contact.'.$key, array_keys($rules)),
+            array_values($rules)
+        );
     }
 }
