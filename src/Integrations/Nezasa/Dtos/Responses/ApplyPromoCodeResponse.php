6f99516b0d9285c8a0341cdf4c6c1c8afe3b85b4
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-26 23:17:45 +0200

    Add promo code entity and methods for calculating discount metrics in ApplyPromoCodeResponse

--- src/Integrations/Nezasa/Dtos/Responses/ApplyPromoCodeResponse.php
+++ src/Integrations/Nezasa/Dtos/Responses/ApplyPromoCodeResponse.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses;
 
 use Nezasa\Checkout\Dtos\BaseDto;
+use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\Entities\PromoCodeResponseEntity;
 use Nezasa\Checkout\Integrations\Nezasa\Dtos\Shared\Price;
 
 class ApplyPromoCodeResponse extends BaseDto
@@ -16,10 +17,22 @@ class ApplyPromoCodeResponse extends BaseDto
         public Price $discountedPackagePrice,
         public Price $packagePrice,
         public Price $promoCodeDiscount,
+        public PromoCodeResponseEntity $promoCode,
     ) {}
 
+    /**
+     * Calculate the percentage decrease in price due to the promo code.
+     */
     public function decreasePercent(): float
     {
         return (($this->packagePrice->amount - $this->discountedPackagePrice->amount) / $this->packagePrice->amount) * 100;
+    }
+
+    /**
+     * Calculate the amount decreased by the promo code.
+     */
+    public function decreaseAmount(): float
+    {
+        return $this->packagePrice->amount - $this->discountedPackagePrice->amount;
     }
 }
