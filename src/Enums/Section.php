ece84c03c4e6e67c5484c779e9f4fbbdfffa392b
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-28 12:24:59 +0200

    Add Section enum to represent different sections in the checkout process

---
+++ src/Enums/Section.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Enums;
+
+use AzimKordpour\PowerEnum\Traits\PowerEnum;
+
+/**
+ * Enum representing different sections in the checkout process.
+ *
+ * @method bool isPromo()
+ * @method bool isContact()
+ * @method bool isTraveler()
+ */
+enum Section: string
+{
+    use PowerEnum;
+
+    case Promo = 'promo';
+    case Contact = 'contact';
+    case Traveler = 'traveler';
+}
