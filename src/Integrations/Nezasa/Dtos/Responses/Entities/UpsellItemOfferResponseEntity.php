86135377619654534cb6beb4f8c0479524b009dc
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-07-31 16:23:59 +0200

    Add serviceCategories property to UpsellItemOfferResponseEntity and create UpsellServiceCategoryResponseDto for enhanced upsell item data structure

--- src/Integrations/Nezasa/Dtos/Responses/Entities/UpsellItemOfferResponseEntity.php
+++ src/Integrations/Nezasa/Dtos/Responses/Entities/UpsellItemOfferResponseEntity.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\Entities;
 
+use Illuminate\Support\Collection;
 use Nezasa\Checkout\Dtos\BaseDto;
 
 class UpsellItemOfferResponseEntity extends BaseDto
@@ -13,6 +14,8 @@ class UpsellItemOfferResponseEntity extends BaseDto
      *
      * @see https://docs.dev.tripbuilder.app/Mo9reezaehiengah/checkout-api-v1.html#tag/Upsell-Items/paths/~1checkout~1v1~1checkouts~1%7BcheckoutId%7D~1upsell-items~1offers/get
      *
+     * @param  Collection<int, UpsellServiceCategoryResponseDto>  $serviceCategories
+     *
      * @note There are other properties in the response, but we only need the ones defined here.
      */
     public function __construct(
@@ -20,6 +23,7 @@ class UpsellItemOfferResponseEntity extends BaseDto
         public string $productId,
         public string $name,
         public string $description,
+        public Collection $serviceCategories,
         public bool $optOutPossible = false,
         public ?string $defaultSelection = null,
     ) {}
