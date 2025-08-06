99dea1cc9494222e1dfee2399866ada80aca8fbf
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-06 09:42:46 +0200

    Add AddedUpsellItemResponseEntity class for handling upsell item responses

---
+++ src/Integrations/Nezasa/Dtos/Responses/Entities/AddedUpsellItemResponseEntity.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\Entities;
+
+use Nezasa\Checkout\Dtos\BaseDto;
+use Nezasa\Checkout\Integrations\Nezasa\Dtos\Shared\Price;
+
+class AddedUpsellItemResponseEntity extends BaseDto
+{
+    /**
+     * Create a new instance of AddedUpsellItemResponseEntity.
+     *
+     * @see https://docs.tripbuilder.app/Mo9reezaehiengah/checkout-api-v1.html#tag/Upsell-Items/paths/~1checkout~1v1~1checkouts~1%7BcheckoutId%7D~1upsell-items~1offers/post
+     *
+     * @note there are other properties in the response that are not included here.
+     */
+    public function __construct(
+        public string $componentRefId,
+        public string $productRefId,
+        public string $serviceCategoryRefId,
+        public string $name,
+        public Price $salesPrice,
+
+    ) {}
+}
