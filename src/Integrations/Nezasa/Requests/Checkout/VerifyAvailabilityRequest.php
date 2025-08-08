ff8f1a930aec51aaab00f8eeac346417f0324b03
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-08 10:49:34 +0200

    Add VerifyAvailabilityRequest for handling availability check requests

---
+++ src/Integrations/Nezasa/Requests/Checkout/VerifyAvailabilityRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Integrations\Nezasa\Requests\Checkout;
+
+use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\VerifyAvailabilityResponse;
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
+use Saloon\Traits\Body\HasJsonBody;
+
+class VerifyAvailabilityRequest extends Request implements HasBody
+{
+    use HasJsonBody;
+
+    /**
+     * Define the HTTP method.
+     */
+    protected Method $method = Method::POST;
+
+    /**
+     * Create a new instance of VerifyAvailabilityRequest
+     */
+    public function __construct(public readonly string $checkoutId) {}
+
+    /**
+     * Define the endpoint for the request.
+     */
+    public function resolveEndpoint(): string
+    {
+        return "/checkout/v1/checkouts/$this->checkoutId/availability-check";
+    }
+
+    /**
+     * Cast the response to a DTO.
+     */
+    public function createDtoFromResponse(Response $response): VerifyAvailabilityResponse
+    {
+        return VerifyAvailabilityResponse::from($response->array());
+    }
+}
