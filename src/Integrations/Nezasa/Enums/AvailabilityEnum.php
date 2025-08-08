cc9497865a07a6ef3d099d8ca59e81c7674bf88c
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-08 10:21:30 +0200

    Add AvailabilityEnum for representing component availability status in checkout process

---
+++ src/Integrations/Nezasa/Enums/AvailabilityEnum.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Integrations\Nezasa\Enums;
+
+use AzimKordpour\PowerEnum\Traits\PowerEnum;
+
+/**
+ * Enum representing the availability status of a component in the checkout process.
+ *
+ * @see https://docs.tripbuilder.app/Mo9reezaehiengah/checkout-api-v1.html#tag/Availability/paths/~1checkout~1v1~1checkouts~1%7BcheckoutId%7D~1availability-check/post
+ *
+ * @method bool isOpen()
+ * @method bool isOnRequest()
+ * @method bool isBooked()
+ * @method bool isNoneBookable()
+ * @method bool isCancelled()
+ * @method bool isNone()
+ */
+enum AvailabilityEnum: string
+{
+    use PowerEnum;
+
+    case Open = 'Open';
+    case OnRequest = 'OnRequest';
+    case Booked = 'Booked';
+    case NoneBookable = 'NoneBookable';
+    case Cancelled = 'Cancelled';
+    case None = 'None';
+}
