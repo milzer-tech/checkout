05c879ae3200248d6274b42eae77b3bad3ffad69
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-11 16:46:57 +0200

    Add PaymentOptions case to Section enum

--- src/Enums/Section.php
+++ src/Enums/Section.php
@@ -24,4 +24,5 @@ enum Section: string
     case Traveller = 'traveller';
     case AdditionalService = 'additional_service';
     case Summary = 'summary';
+    case PaymentOptions = 'payment-options';
 }
