250f57bdc35cf64b986089d1bd11196033b4e26c
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-11 15:36:21 +0200

    Update availability status checks to include booked status in views

--- src/Resources/Views/components/availability-status.blade.php
+++ src/Resources/Views/components/availability-status.blade.php
@@ -1,4 +1,4 @@
-@if($availability?->isOpen())
+@if($availability?->isOpen() || $availability?->isBooked())
     <svg class="w-4 h-4 mr-2 text-green-600 dark:text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
     </svg>
@@ -11,12 +11,12 @@
 
 @endif
 
-@if($availability?->isBooked())
-    <svg class="w-4 h-4 mr-2 text-yellow-500 dark:text-yellow-400" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12 4v8" stroke="currentColor" stroke-width="3" stroke-linecap="round"/>
-        <circle cx="12" cy="17" r="2" fill="currentColor"/>
-    </svg>
-@endif
+{{--@if($availability?->isBooked())--}}
+{{--    <svg class="w-4 h-4 mr-2 text-yellow-500 dark:text-yellow-400" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">--}}
+{{--        <path d="M12 4v8" stroke="currentColor" stroke-width="3" stroke-linecap="round"/>--}}
+{{--        <circle cx="12" cy="17" r="2" fill="currentColor"/>--}}
+{{--    </svg>--}}
+{{--@endif--}}
 
 @if($availability?->isOnRequest())
     <svg class="w-4 h-4 mr-2 text-blue-500 dark:text-blue-400" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
--- src/Resources/Views/components/available.blade.php
+++ src/Resources/Views/components/available.blade.php
@@ -1,5 +1,5 @@
 
-@if($availability?->isOpen())
+@if($availability?->isOpen() || $availability?->isBooked())
 <span class="inline-flex items-center px-3 py-1 bg-[#F2FCE2] dark:bg-green-900/30 text-green-600 dark:text-green-400 text-sm rounded-full">
     <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
          xmlns="http://www.w3.org/2000/svg">
