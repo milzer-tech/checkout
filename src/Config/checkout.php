94f331b3d10dc032a07b7e9aab075df1b91bae53
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-12 16:06:20 +0200

    Add Oppwa payment configuration to checkout.php

--- src/Config/checkout.php
+++ src/Config/checkout.php
@@ -10,4 +10,13 @@ return [
         'username' => env('CHECKOUT_NEZASA_USERNAME', 'your_username'),
         'password' => env('CHECKOUT_NEZASA_PASSWORD', 'your_password'),
     ],
+
+    'payment' => [
+        'oppwa' => [
+            'name' => env('CHECKOUT_OPPWA_NAME', 'oppwa'),
+            'base_url' => env('CHECKOUT_OPPWA_BASE_URL', 'https://eu-test.oppwa.com'),
+            'entity_id' => env('CHECKOUT_OPPWA_ENTITY_ID', '8a8294184e736012014e78c4c4cb17dc'),
+            'token' => env('CHECKOUT_OPPWA_TOKEN', 'OGE4Mjk0MTg0ZTczNjAxMjAxNGU3OGM0YzRlNDE3ZTB8NHRKQ21qMkJ0Mw=='),
+        ],
+    ],
 ];
