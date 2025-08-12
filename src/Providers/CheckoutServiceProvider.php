20f11f20ef1f548c53c033a8554447c1fbc00bb5
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-12 16:06:10 +0200

    Add PaymentPage component and update date format configuration in CheckoutServiceProvider

--- src/Providers/CheckoutServiceProvider.php
+++ src/Providers/CheckoutServiceProvider.php
@@ -11,6 +11,7 @@ use Nezasa\Checkout\Livewire\AdditionalServicesSection;
 use Nezasa\Checkout\Livewire\Banner;
 use Nezasa\Checkout\Livewire\ContactDetails;
 use Nezasa\Checkout\Livewire\PaymentOptionsSection;
+use Nezasa\Checkout\Livewire\PaymentPage;
 use Nezasa\Checkout\Livewire\PromoCodeSection;
 use Nezasa\Checkout\Livewire\Stepper;
 use Nezasa\Checkout\Livewire\TravelerDetails;
@@ -57,6 +58,7 @@ class CheckoutServiceProvider extends ServiceProvider
         Livewire::component(name: 'payment-options-section', class: PaymentOptionsSection::class);
         Livewire::component(name: 'trip-summary', class: TripSummary::class);
         Livewire::component(name: 'trip-details-page', class: TripDetailsPage::class);
+        Livewire::component(name: 'payment-page', class: PaymentPage::class);
     }
 
     /**
@@ -78,7 +80,7 @@ class CheckoutServiceProvider extends ServiceProvider
     {
         Config::set(key: 'livewire.layout', value: 'checkout::layouts.layout');
 
-        Config::set(key: 'data.date_format', value: [DATE_ATOM, 'Y-m-d', 'Y-m-d\TH:i:s.uP']);
+        Config::set(key: 'data.date_format', value: [DATE_ATOM, 'Y-m-d', 'Y-m-d\TH:i:s.uP', 'Y-m-d H:i:sO']);
     }
 
     /**
