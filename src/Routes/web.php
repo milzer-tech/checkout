1900bc1929602d4cefb0dec2655af542a8fc7c84
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-13 16:49:18 +0200

    Add signature validation middleware to payment route

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
+use Illuminate\Routing\Middleware\ValidateSignature;
 use Illuminate\Support\Facades\Route;
 use Nezasa\Checkout\Livewire\PaymentPage;
 use Nezasa\Checkout\Livewire\PaymentResult;
 use Nezasa\Checkout\Livewire\TripDetailsPage;
 
-Route::get('', TripDetailsPage::class)->name('traveler-details');
-Route::get('payment', PaymentPage::class)->name('payment');
-Route::get('payment-result', PaymentResult::class)->name('payment-result');
+Route::get('', TripDetailsPage::class)
+    ->name('traveler-details');
+
+Route::get('payment', PaymentPage::class)
+    ->middleware(ValidateSignature::class)
+    ->name('payment');
+
+Route::get('payment-result', PaymentResult::class)
+    ->name('payment-result');
