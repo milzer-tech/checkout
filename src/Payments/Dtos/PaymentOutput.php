cd8159fa5c022812019d2af36d54e583a9c8758f
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-13 15:13:04 +0200

    Add PaymentOutput DTO to represent payment processing output

---
+++ src/Payments/Dtos/PaymentOutput.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Payments\Dtos;
+
+use Nezasa\Checkout\Dtos\BaseDto;
+use Nezasa\Checkout\Payments\Enums\PaymentGatewayEnum;
+use Nezasa\Checkout\Payments\Enums\PaymentStatusEnum;
+
+class PaymentOutput extends BaseDto
+{
+    /**
+     * Create a new instance of PaymentOutput.
+     */
+    public function __construct(
+        public PaymentGatewayEnum $gateway,
+        public PaymentStatusEnum $status,
+        public array|BaseDto $data = [],
+    ) {}
+}
