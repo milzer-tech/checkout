5654c3311d191312969f81d5138be897b1945ed2
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-13 15:13:30 +0200

    Refactor payment options section and enhance payment page layout

--- src/Resources/Views/trip-details-page/payment-page.blade.php
+++ src/Resources/Views/trip-details-page/payment-page.blade.php
@@ -1,4 +1,6 @@
 <div class="flex flex-col min-h-screen">
+
+
     <div class="flex-grow grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div class="lg:col-span-2 space-y-6">
             <section>
@@ -7,35 +9,66 @@
                 <div class="p-8 border border-gray-200 rounded-lg bg-white dark:bg-gray-800 shadow-sm">
                     <div class="flex items-center justify-center h-48">
                         <p class="text-lg text-gray-700 dark:text-gray-300">
-                            External payment method will open
-                        </p>
+
+                            @if($payment->isAvailable)
+                                @foreach($payment->scripts as $script)
+                                    {!! $script !!}
+                                @endforeach
+
+                                {!! $payment->html !!}
+                            @endif
+
+                            </p>
+
+                        @unless($payment->isAvailable)
+                            <div class="flex flex-col items-center justify-center h-48 text-center">
+                                <p class="text-lg text-gray-700 dark:text-gray-300">
+                                    The selected payment method is not available.<br>
+                                    Please choose another payment method.
+                                </p>
+                                <a href="{{ route('traveler-details', $this->getQueryParams()) }}"
+                                    class="mt-8 px-5 py-2 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition">
+                                    Go Back
+                                </a>
+                            </div>
+
+                        @endunless
                     </div>
                 </div>
             </section>
         </div>
 
         <div class="lg:col-span-1">
-            <livewire:trip-summary />
+            <livewire:trip-summary
+                :$itinerary
+                :$model
+                :is-completed="$model->data['status']['summary']['isCompleted']"
+                :is-expanded="false"
+                :traveller-processed="$model->data['status']['traveller']['isCompleted']"
+            />
         </div>
     </div>
 
     <!-- Footer with navigation and copyright -->
-    <div class="mt-10 mb-6">
+    <div class=" mb-6">
         <div class="flex justify-between items-center">
-            <button wire:click="goBack" class="flex items-center gap-2 px-6 py-3 rounded-md border border-gray-300 dark:border-gray-600">
-                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <button wire:click="goBack()"
+                    class="flex items-center gap-2 px-6 py-3 rounded-md border border-gray-300 dark:border-gray-600">
+                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                     xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
                 </svg>
                 Back
             </button>
 
-            <button wire:click="goToPayment" class="bg-blue-500 hover:bg-blue-600 text-white px-8 py-3 rounded-md">
-                Pay {{ $totalPrice }} € (EUR)
-            </button>
+            {{--            <button wire:click="goToPayment" class="bg-blue-500 hover:bg-blue-600 text-white px-8 py-3 rounded-md">--}}
+            {{--                Pay {{ $totalPrice }} € (EUR)--}}
+            {{--            </button>--}}
         </div>
 
         <div class="text-center mt-10 text-gray-500 dark:text-gray-400">
             Copyright 2025 Squad Ruby Tours. All rights reserved
         </div>
     </div>
+
 </div>
