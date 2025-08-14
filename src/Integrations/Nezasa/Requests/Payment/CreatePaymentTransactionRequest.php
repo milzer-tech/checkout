a3e0d380d967b090895afed094079110f146149b
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-14 18:33:40 +0200

    Add nezasa_transaction fields to checkout table for transaction handling

--- src/Integrations/Nezasa/Requests/Payment/CreatePaymentTransactionRequest.php
+++ src/Integrations/Nezasa/Requests/Payment/CreatePaymentTransactionRequest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Nezasa\Checkout\Integrations\Nezasa\Requests\Payment;
 
 use Nezasa\Checkout\Integrations\Nezasa\Dtos\Payloads\CreatePaymentTransactionPayload;
+use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\CreatePaymentTransactionResponse;
 use Saloon\Contracts\Body\HasBody;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
@@ -33,7 +34,7 @@ class CreatePaymentTransactionRequest extends Request implements HasBody
      */
     public function resolveEndpoint(): string
     {
-        return "payment-transactions/v1/$this->checkoutRefId/create";
+        return 'payment-transactions/v1.13/'.$this->checkoutRefId.'/create';
     }
 
     /**
@@ -43,8 +44,18 @@ class CreatePaymentTransactionRequest extends Request implements HasBody
      */
     protected function defaultBody(): array
     {
-        return $this->payload->toArray();
+        $data = $this->payload->toArray();
+
+        $data['amount']['amount'] = $data['amount']['value'] = $this->payload->amount->getPaymentAmount();
+
+        return $data;
     }
 
-    public function createDtoFromResponse(Response $response): mixed {}
+    /**
+     * Create a DTO from the response.
+     */
+    public function createDtoFromResponse(Response $response): CreatePaymentTransactionResponse
+    {
+        return CreatePaymentTransactionResponse::from($response->array());
+    }
 }
