b663359ab86a03d238c0ecb91099e31684c33b38
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-26 10:09:33 +0200

    Set default value of checkingAvailability to false in TripDetailsPage

--- src/Livewire/TripDetailsPage.php
+++ src/Livewire/TripDetailsPage.php
@@ -115,6 +115,6 @@ class TripDetailsPage extends BaseCheckoutComponent
             );
         }
 
-        $this->checkingAvailability = null;
+        $this->checkingAvailability = false;
     }
 }
