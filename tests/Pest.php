c22a7c10e0546c45896790e4b3fcb57f5dc78949
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-08-28 15:18:12 +0200

    Add 404 response JSON files and mock responses for error handling

--- tests/Pest.php
+++ tests/Pest.php
@@ -45,3 +45,17 @@ function fakeInitialNezasaCalls(): void
         CountriesRequest::class => MockResponse::fixture('countries_response'),
     ]);
 }
+
+function fakeInitialNotFoundNezasaCalls(): void
+{
+    MockClient::global([
+        GetItineraryRequest::class => MockResponse::fixture('get_itinerary_404_response'),
+        RetrieveCheckoutRequest::class => MockResponse::fixture('retrieve_checkout_404_response'),
+        AddedRentalCarsRequest::class => MockResponse::fixture('added_rental_cars_404_response'),
+        TravelerRequirementsRequest::class => MockResponse::fixture('traveller_requirements_404_response'),
+        GetAvailableUpsellItemsRequest::class => MockResponse::fixture('get_available_upsell_items_404_response'),
+        RetrieveCheckoutUpsellItemsRequest::class => MockResponse::fixture('retrieve_checkout_upsell_items_404_response'),
+        CountryCodesRequest::class => MockResponse::fixture('country_codes_404_response'),
+        CountriesRequest::class => MockResponse::fixture('countries_404_response'),
+    ]);
+}
