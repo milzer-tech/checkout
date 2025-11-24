3b202478f26dba394f96018101cec10d91d8ee85
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-11-24 15:26:22 +0100

    Refactor `ActivitySection` to include dynamic activity questions handling, validation rules, and enhanced rendering logic in blade template.

--- src/Livewire/ActivitySection.php
+++ src/Livewire/ActivitySection.php
@@ -3,7 +3,14 @@
 namespace Nezasa\Checkout\Livewire;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
+use Illuminate\Validation\Rule;
+use Livewire\Attributes\On;
 use Nezasa\Checkout\Enums\Section;
+use Nezasa\Checkout\Integrations\Nezasa\Connectors\NezasaConnector;
+use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\ActivityQuestionResponse;
+use Nezasa\Checkout\Integrations\Nezasa\Enums\AnswerInputEnum;
+use Nezasa\Checkout\Jobs\VerifyAvailabilityJob;
 
 class ActivitySection extends BaseCheckoutComponent
 {
@@ -13,11 +20,29 @@ class ActivitySection extends BaseCheckoutComponent
     public bool $shouldRender = false;
 
     /**
+     * The list of activity questions.
+     *
+     * @var Collection<int, ActivityQuestionResponse>
+     */
+    public Collection $activityQuestions;
+
+    public array $result = [];
+
+    /**
      * Mount the component/
      */
     public function mount(bool $shouldRender = false): void
     {
-        $this->shouldRender = false;
+        $this->shouldRender = $shouldRender;
+
+        $this->activityQuestions = new Collection;
+
+        if (
+            $this->model->data['status']['traveller']['isCompleted']
+            && ! $this->model->data['status']['activity']['isCompleted']
+        ) {
+            $this->listen();
+        }
     }
 
     /**
@@ -34,5 +59,71 @@ class ActivitySection extends BaseCheckoutComponent
     public function next(): void
     {
         $this->markAsCompletedAdnCollapse(Section::Activity);
+    }
+
+    #[On(Section::Activity->value)]
+    public function listen(): void
+    {
+        $verifyAvailability = new VerifyAvailabilityJob($this->checkoutId);
+
+        if ($this->shouldRender) {
+            $verifyAvailability->handle();
+
+            $this->activityQuestions = NezasaConnector::make()
+                ->checkout()
+                ->getActivityQuestions($this->checkoutId)
+                ->dto();
+
+            foreach ($this->activityQuestions as $component) {
+                foreach ($component->questions as $question) {
+                    $this->result[$component->componentId][$question->refId] = null;
+                }
+            }
+        } else {
+            dispatch($verifyAvailability);
+
+            $this->dispatch(Section::Promo->value);
+        }
+    }
+
+    public function updated($property, $value): void
+    {
+        [$componentId, $questionId] = str($property)->after('.')->explode('.');
+
+        $this->validate(
+            rules: [
+                $property => $this->rules()['result'][$componentId][$questionId],
+            ],
+            attributes: [
+                $property => str($property)->afterLast('.')->toString(),
+            ]
+        );
+    }
+
+    protected function rules(): array
+    {
+        $rules = [];
+
+        foreach ($this->activityQuestions as $component) {
+            foreach ($component->questions as $question) {
+                $propertyRules = $question->required ? ['required'] : ['nullable'];
+
+                switch ($question->getInputType()) {
+                    case AnswerInputEnum::Select:
+                        $propertyRules[] = Rule::in($question->answerOptions->pluck('refId')->flatten());
+                        break;
+
+                    case AnswerInputEnum::Radio:
+                        $propertyRules[] = 'boolean';
+                        break;
+                    default:
+                        $propertyRules[] = 'sometimes';
+                }
+
+                $rules['result'][$component->componentId][$question->refId] = $propertyRules;
+            }
+        }
+
+        return $rules;
     }
 }
--- src/Resources/Views/blades/activity-section.blade.php
+++ src/Resources/Views/blades/activity-section.blade.php
@@ -1,7 +1,10 @@
 @use(Nezasa\Checkout\Enums\Section)
+@use(Nezasa\Checkout\Integrations\Nezasa\Enums\AnswerInputEnum)
+@use(Nezasa\Checkout\Supporters\AutoCompleteSupporter)
+
 @php($state = $isExpanded ? 'editing' : 'valid')
 
-@if($shouldRender)
+{{--@if($shouldRender)--}}
     <x-checkout::editable-box
         title="{{trans('checkout::page.trip_details.activity_details')}}"
         :state="$state"
@@ -9,28 +12,71 @@
         :showCheck="$isCompleted"
         onEdit="expand('{{Section::Activity->value}}')"
     >
-        <div class="space-y-4">
+
+        @foreach($activityQuestions as $componentIndex =>  $component)
+            <div class="py-2 bg-white dark:bg-gray-800">
+                <h2 class="text-xl font-semibold mb-8">{{$component->productName}}</h2>
+
+                <div id="{{$component->componentId}}">
+                    @foreach($component->questions as $questionIndex => $question)
+                        <div class="grid grid-cols-1 lg:grid-cols-1 gap-6 min-w-0 mb-5">
+                            <div class="space-y-2 w-full min-w-0">
+                                <label
+                                    class="block text-gray-700 dark:text-gray-200 font-medium overflow-ellipsis whitespace-nowrap overflow-hidden">
+                                    {{$question->question}} @if($question->required)
+                                        ({{trans('checkout::page.trip_details.required')}})
+                                    @endif
+                                </label>
+
+                                @switch($question->getInputType())
+                                    @case(AnswerInputEnum::Select)
+                                        <select name="result.{{$component->componentId}}.{{$question->refId}}"
+                                                wire:model.change="result.{{$component->componentId}}.{{$question->refId}}"
+                                                class="form-select pr-8 w-full">
+                                            <option>Select</option>
+                                            @foreach($question->answerOptions as $option)
+                                                <option value="{{$option->refId}}">{{$option->displayName}}</option>
+                                            @endforeach
+                                        </select>
+                                        @break
+                                    @case(AnswerInputEnum::Radio)
+                                        @break
+                                    @default
+                                        <input
+                                            name="contact.firstName"
+                                            type="text"
+                                            @if($question->placeholder) placeholder="{{$question->placeholder}}" @endif
+                                            wire:model.blur="contact.firstName"
+                                            class="form-input w-full">
+                                @endswitch
+                                @error("result.$component->componentId.$question->refId")<span class="text-red-500 text-sm">{{ $message }}</span>@enderror
+
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="h-px bg-gray-200 dark:bg-gray-700 -mx-8 mb-8"></div>
+                @endforeach
+
+                <div class="h-px bg-gray-200 dark:bg-gray-700 -mx-8"></div>
+
+                <div class="flex justify-between items-center mt-8">
+                    <button type="button"
+                            wire:click="next"
+                            class="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md">
+                        {{trans('checkout::page.trip_details.back')}}
+                    </button>
+
+                    <button type="button"
+                            wire:click="next"
+                            class="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md">
+                        {{trans('checkout::page.trip_details.next')}}
+                    </button>
+                </div>
 
 
-            <div class="h-px bg-gray-200 dark:bg-gray-700 -mx-8"></div>
-
-            <div class="flex justify-between items-center">
-                <button type="button"
-                        wire:click="next"
-                        class="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md">
-                    {{trans('checkout::page.trip_details.back')}}
-                </button>
-
-                <button type="button"
-                        wire:click="next"
-                        class="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md">
-                    {{trans('checkout::page.trip_details.next')}}
-                </button>
-            </div>
-
-        </div>
     </x-checkout::editable-box>
-@else
+{{--@else--}}
 
-<div class="invisible"></div>
-@endif
+{{--    <div class="invisible"></div>--}}
+{{--@endif--}}
