e7581446b4bd589514b7a5678ea6cda76f0d49a1
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-11-24 15:28:48 +0100

    Refactor `PromoCodeSection` to remove `VerifyAvailabilityJob` dispatch and update event listener to use `Section::Promo->value`.

--- src/Integrations/Nezasa/Requests/Checkout/GetActivityQuestionsRequest.php
+++ src/Integrations/Nezasa/Requests/Checkout/GetActivityQuestionsRequest.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace Nezasa\Checkout\Integrations\Nezasa\Requests\Checkout;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Config;
 use Nezasa\Checkout\Exceptions\NotFoundException;
 use Nezasa\Checkout\Integrations\Nezasa\Dtos\Responses\ActivityQuestionResponse;
 use Saloon\Enums\Method;
+use Saloon\Helpers\MiddlewarePipeline;
+use Saloon\Http\Faking\FakeResponse;
+use Saloon\Http\PendingRequest;
 use Saloon\Http\Request;
 use Saloon\Http\Response;
 use Throwable;
@@ -47,5 +51,28 @@ class GetActivityQuestionsRequest extends Request
         return collect(
             ActivityQuestionResponse::collect($response->array())
         );
+    }
+
+    public function middleware(): MiddlewarePipeline
+    {
+        if (! Config::boolean('checkout.fake_calls')) {
+            return parent::middleware();
+        }
+
+        return parent::middleware()
+            ->onRequest(function (PendingRequest $pendingRequest) {
+                $file = file_get_contents(
+                    checkout_path('tests/Fixtures/Saloon/get_activity_question_response.json')
+                );
+
+                if ($file === false) {
+                    return new FakeResponse('fake file not found: get_activity_question_response.json');
+                }
+
+                $data = json_decode($file, true);
+
+                /** @phpstan-ignore-next-line */
+                return new FakeResponse($data['data'], $data['statusCode'], $data['headers']);
+            });
     }
 }
