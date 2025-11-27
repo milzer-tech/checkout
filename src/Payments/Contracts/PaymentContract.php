2eb572ae05bf2691e27ba8f6eb84e556265bd944
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-11-27 10:39:28 +0100

    Add `PaymentContract` interface to define standard methods for payment gateway integrations

---
+++ src/Payments/Contracts/PaymentContract.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Payments\Contracts;
+
+use Illuminate\Http\Request;
+use Nezasa\Checkout\Dtos\BaseDto;
+use Nezasa\Checkout\Integrations\Nezasa\Dtos\Payloads\CreatePaymentTransactionPayload as NezasaPayload;
+use Nezasa\Checkout\Payments\Dtos\PaymentInit;
+use Nezasa\Checkout\Payments\Dtos\PaymentOutput;
+use Nezasa\Checkout\Payments\Dtos\PaymentPrepareData;
+use Nezasa\Checkout\Payments\Dtos\PaymentResult;
+
+interface PaymentContract
+{
+    /**
+     * Returns whether the payment gateway is active.
+     */
+    public static function isActive(): bool;
+
+    /**
+     * Returns the name of the payment gateway.
+     */
+    public static function name(): string;
+
+    /**
+     * Prepares the payment initiation process.
+     */
+    public function prepare(PaymentPrepareData $data): PaymentInit;
+
+    /**
+     * Returns the payload required for creating a transaction in Nezasa.
+     */
+    public function makeNezasaTransactionPayload(PaymentPrepareData $data, PaymentInit $paymentInit): NezasaPayload;
+
+    /**
+     * Handles the callback from the payment gateway.
+     *
+     * @param  array<string, mixed>|BaseDto  $persistentData
+     */
+    public function verify(Request $request, array|BaseDto $persistentData): PaymentResult;
+
+    /**
+     * Shows the result of the payment process to the user.
+     */
+    public function output(PaymentResult $result, PaymentOutput $output): PaymentOutput;
+}
