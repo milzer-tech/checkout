8d2f8563828b84762d2321fe8b552a72de4245bd
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-12-05 09:53:06 +0100

    Update `CheckoutEventServiceProvider` to register listeners for itinerary booking success and failure events.

--- config/cuba-travel.php
+++ config/cuba-travel.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
+    'email' => [
+        'from_name' => env('CUBA_TRAVEL_EMAIL_FROM_NAME', 'Checkout application'),
+        'from' => env('CUBA_TRAVEL_FROM_EMAIL', 'no-reply@test.com'),
+        'to' => json_decode(env('CUBA_TRAVEL_TO_EMAILS', '["me@test.com", "they@test.com"]')),
+    ],
+
     'active' => (bool) env('CUBA_TRAVEL_ACTIVE', false),
 
-    'reasons' => [
-        'for fun',
-        'visit family',
-        'new york city',
-    ],
+    'reasons' => json_decode(env('CUBA_TRAVEL_REASONS', '["holiday trip","beach vacation","sightseeing","visiting friends","attending a wedding","work conference","studying abroad","medical treatment"]')),
 ];
