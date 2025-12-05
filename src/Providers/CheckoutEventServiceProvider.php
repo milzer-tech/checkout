4d45b3f3a16fe473755067e44d8b97275324f398
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-12-05 09:52:56 +0100

    Update `CheckoutEventServiceProvider` to register listeners for itinerary booking success and failure events.

--- src/Providers/CheckoutEventServiceProvider.php
+++ src/Providers/CheckoutEventServiceProvider.php
@@ -6,7 +6,8 @@ namespace Nezasa\Checkout\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider;
 use Illuminate\Support\Facades\Event;
-use Nezasa\Checkout\Events\ItineraryBooked;
+use Nezasa\Checkout\Events\ItineraryBookingFailedEvent;
+use Nezasa\Checkout\Events\ItineraryBookingSucceededEvent;
 use Nezasa\Checkout\Listeners\SendEmailToCubaTravelListener;
 
 class CheckoutEventServiceProvider extends EventServiceProvider
@@ -16,6 +17,8 @@ class CheckoutEventServiceProvider extends EventServiceProvider
      */
     public function boot(): void
     {
-        Event::listen(ItineraryBooked::class, SendEmailToCubaTravelListener::class);
+        Event::listen(ItineraryBookingSucceededEvent::class, SendEmailToCubaTravelListener::class);
+
+        Event::listen(ItineraryBookingFailedEvent::class, SendEmailToCubaTravelListener::class);
     }
 }
