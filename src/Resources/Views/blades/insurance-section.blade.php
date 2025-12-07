1cd5a2e9d3ad2ca5ed657558e9bf420b61c0a5ec
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-12-07 20:26:51 +0100

    Enhance `insurance-section` blade to support `checkout.insurance.vertical.active` configuration, update dynamic traveler data population, and refine conditional offer rendering logic.

--- src/Resources/Views/blades/insurance-section.blade.php
+++ src/Resources/Views/blades/insurance-section.blade.php
@@ -1,4 +1,6 @@
-@use(Nezasa\Checkout\Enums\Section;use Nezasa\Checkout\Supporters\InsuranceSupporter)
+@use(Nezasa\Checkout\Enums\Section)
+@use(Illuminate\Support\Facades\Config)
+@use(Nezasa\Checkout\Supporters\InsuranceSupporter)
 @php($state = $isExpanded ? 'editing' : 'valid')
 
 <x-checkout::editable-box
@@ -13,46 +15,50 @@
     <div class="space-y-4">
 
         @if(InsuranceSupporter::isAvailable() && $contact)
-            @push('scripts')
-                <script src="https://cdn.jsdelivr.net/npm/@vertical-insure/embedded-offer"></script>
-            @endpush
-            <div id="insurance_offer" wire:ignore wire:key="insurance_offer"></div>
 
-            <div wire:ignore wire:key="insurance_script">
-                <script>
-                    new VerticalInsure("#insurance_offer", {
-                        client_id: "{{config()->string('checkout.insurance.vertical.username')}}",
+            @if(Config::boolean('checkout.insurance.vertical.active'))
+                @push('scripts')
+                    <script src="https://cdn.jsdelivr.net/npm/@vertical-insure/embedded-offer"></script>
+                @endpush
+                <div id="insurance_offer" wire:ignore wire:key="insurance_offer"></div>
+                <div wire:ignore wire:key="insurance_script">
+                    <script>
+                        new VerticalInsure("#insurance_offer", {
+                            client_id: "{{config()->string('checkout.insurance.vertical.username')}}",
 
-                        product_config: {
-                            "travel": [{
-                                "customer": {
-                                    "first_name": "{{$contact->firstName}}",
-                                    "last_name": "{{$contact->lastName}}",
-                                    "email_address": "{{$contact->email}}"
-                                },
-                                "attributes": {
-                                    "trip_start_date": "{{$itinerary->startDate->toDateString()}}",
-                                    "trip_end_date": "{{$itinerary->endDate->toDateString()}}",
-                                    "destination_countries": {!! $itinerary->destinationCountries->toJson() !!},
-                                    "trip_cost": {{$itinerary->price->discountedPackagePrice->toCent()}},
-                                    "trip_cost_currency": "{{$itinerary->price->discountedPackagePrice->currency}}"
-                                },
-                                "currency": "{{$itinerary->price->discountedPackagePrice->currency}}"
-                            }],
-                        }
+                            product_config: {
+                                "travel": [{
+                                    "customer": {
+                                        "first_name": "{{$contact->firstName}}",
+                                        "last_name": "{{$contact->lastName}}",
+                                        "email_address": "{{$contact->email}}",
+                                        state: "MN",
+                                        country: "US"
+                                        {{--"street": "{{$contact->address->street1}} {{$contact->address->street2}}",--}}
+                                        {{--"city": "{{$contact->address->city}}",--}}
+                                        {{--"postal_code": "{{$contact->address->postalCode}}",--}}
+                                        {{--"country": "{{str($contact->address->country)->beforeLast('-')->toString()}}"--}}
+                                    },
+                                    "attributes": {
+                                        "trip_start_date": "{{$itinerary->startDate->toDateString()}}",
+                                        "trip_end_date": "{{$itinerary->endDate->toDateString()}}",
+                                        "destination_countries": {!! $itinerary->destinationCountries->toJson() !!},
+                                        "trip_cost": {{$itinerary->price->discountedPackagePrice->toCent()}},
+                                        "trip_cost_currency": "{{$itinerary->price->discountedPackagePrice->currency}}"
+                                    },
+                                    "currency": "{{$itinerary->price->discountedPackagePrice->currency}}"
+                                }],
+                            }
 
-                    }, function (offerState) {
-                        console.log(offerState)
-                    });
+                        });
 
-                    window.addEventListener("offer-state-change", (e) => {
-                        @this.call('handleInsuranceQuotes', e.detail.quotes[0]);
-                    });
-                </script>
-            </div>
-
+                        window.addEventListener("offer-state-change", (e) => {
+                            @this.call('handleInsuranceQuote', e.detail.quotes[0]);
+                        });
+                    </script>
+                </div>
+            @endif
         @endif
-
 
         <div class="space-y-4 mt-8">
             <div class="h-px bg-gray-200 dark:bg-gray-700 -mx-8"></div>
