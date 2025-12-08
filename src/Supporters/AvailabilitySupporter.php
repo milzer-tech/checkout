70d8deada6998f2bc1989bd8a1679089a045bbb5
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2025-12-08 15:40:08 +0100

    Refactor `AvailabilitySupporter` to add caching methods with doc comments and improve code clarity.

--- src/Supporters/AvailabilitySupporter.php
+++ src/Supporters/AvailabilitySupporter.php
@@ -13,6 +13,9 @@ final class AvailabilitySupporter
 
     private static string $statusKeyPrefix = 'varifyAvailability-status-';
 
+    /**
+     * Cache the result of the availability verification.
+     */
     public function cacheResult(string $checkoutId, Response $response): void
     {
         Cache::put(
@@ -28,22 +31,37 @@ final class AvailabilitySupporter
         );
     }
 
+    /**
+     * Get the cached result of the availability verification.
+     *
+     * @return array<string, mixed>
+     */
     public function getCachedResult(string $checkoutId): array
     {
         return Cache::get(self::$keyPrefix.$checkoutId, []);
     }
 
+    /**
+     * Get the cached status of the availability verification.
+     */
     public function getCachedStatus(string $checkoutId): int
     {
         return (int) Cache::get(self::$statusKeyPrefix.$checkoutId, 500);
     }
 
+    /**
+     * Clear the cached result and status of the availability verification.
+     */
     public function clearCache(string $checkoutId): void
     {
         Cache::forget(self::$keyPrefix.$checkoutId);
+
         Cache::forget(self::$statusKeyPrefix.$checkoutId);
     }
 
+    /**
+     * Check if the availability verification result and status are cached.
+     */
     public function has(string $checkoutId): bool
     {
         return Cache::has(self::$keyPrefix.$checkoutId) && Cache::has(self::$statusKeyPrefix.$checkoutId);
