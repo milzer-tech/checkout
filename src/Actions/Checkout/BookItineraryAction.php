e11af515cf7c5cf82e1962f9a8dba41e012bc94c
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-01-30 13:16:50 +0100

    Refactor `BookItineraryAction` to return `Response` instead of `BookingStatusEnum` by simplifying the booking flow

--- src/Actions/Checkout/BookItineraryAction.php
+++ src/Actions/Checkout/BookItineraryAction.php
@@ -5,42 +5,15 @@ declare(strict_types=1);
 namespace Nezasa\Checkout\Actions\Checkout;
 
 use Nezasa\Checkout\Integrations\Nezasa\Connectors\NezasaConnector;
-use Nezasa\Checkout\Payments\Enums\BookingStatusEnum;
-use Throwable;
+use Saloon\Http\Response;
 
 class BookItineraryAction
 {
     /**
      * Handle the booking of the itinerary.
      */
-    public function run(string $checkoutId): BookingStatusEnum
+    public function run(string $checkoutId): Response
     {
-        try {
-            $failures = collect();
-            $successes = collect();
-
-            NezasaConnector::make()->checkout()->synchronousBooking($checkoutId)
-                ->collect('summary.components')
-                ->reject(fn (array $item) => $item['isPlaceholder'])
-                ->each(
-                    fn (array $item) => $item['isBooked'] ? $successes->add($item['id']) : $failures->add($item['id'])
-                );
-
-            if ($successes->isNotEmpty() && $failures->isEmpty()) {
-                return BookingStatusEnum::CompleteSuccess;
-            }
-
-            if ($successes->isEmpty() && $failures->isNotEmpty()) {
-                return BookingStatusEnum::CompleteFailed;
-            }
-
-            if ($successes->isNotEmpty() && $failures->isNotEmpty()) {
-                return BookingStatusEnum::PartialFailure;
-            }
-        } catch (Throwable $exception) {
-            report($exception);
-        }
-
-        return BookingStatusEnum::Unknown;
+        return NezasaConnector::make()->checkout()->synchronousBooking($checkoutId);
     }
 }
