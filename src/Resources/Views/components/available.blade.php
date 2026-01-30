384e9eec4e0f6a6a0d0818befb49564e1b3972e3
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-01-30 16:04:57 +0100

    Update `PaymentResultPage` to handle `availability` state and display "Booked" status; add corresponding language translation.

--- src/Resources/Views/components/available.blade.php
+++ src/Resources/Views/components/available.blade.php
@@ -5,7 +5,11 @@
          xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
     </svg>
-    {{trans('checkout::page.trip_details.available')}}
+    @if($this->getName() === 'payment-result-page' && $availability?->isBooked())
+        {{trans('checkout::page.trip_details.booked')}}
+    @else
+        {{trans('checkout::page.trip_details.available')}}
+    @endif
 </span>
 @endif
 
