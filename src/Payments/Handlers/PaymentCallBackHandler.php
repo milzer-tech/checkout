762732f3f701e67281a973f911c45e2d9b294bd8
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-02-11 13:01:07 +0100

    Suppress phpstan warnings in `PaymentCallBackHandler` for dynamic class instantiation and `getOutput` method.

--- src/Payments/Handlers/PaymentCallBackHandler.php
+++ src/Payments/Handlers/PaymentCallBackHandler.php
@@ -73,6 +73,7 @@ readonly class PaymentCallBackHandler
             ->firstOrFail()
             ->decryptClassName();
 
+        /** @phpstan-ignore-next-line  */
         return new $result;
     }
 
@@ -134,6 +135,7 @@ readonly class PaymentCallBackHandler
      */
     private function getOutput(Transaction $transaction): PaymentOutput
     {
+        /** @phpstan-ignore-next-line  */
         $data = collect($transaction->result_data['nezasa_booking_summary']['components'])
             ->reject(fn (array $item) => $item['isPlaceholder'])
             ->mapwithkeys(fn (array $item): array => [$item['id'] => AvailabilityEnum::tryFrom($item['status'])]);
