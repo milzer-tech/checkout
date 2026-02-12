bb5f695c292e2b78154f89240e4642eb0d439dc9
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-02-12 14:12:07 +0100

    Implement additional payment lifecycle methods in `OppwaWidgetGateway`
    Added support for capture and abort operations, along with enhancements to authorization and preparation workflows. Included new DTO integrations for handling Oppwa payloads.

--- src/Payments/Gateways/Oppwa/OppwaWidgetGateway.php
+++ src/Payments/Gateways/Oppwa/OppwaWidgetGateway.php
@@ -8,34 +8,45 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Config;
-use Nezasa\Checkout\Dtos\BaseDto;
 use Nezasa\Checkout\Integrations\Nezasa\Dtos\Payloads\CreatePaymentTransactionPayload as NezasaPayload;
 use Nezasa\Checkout\Integrations\Nezasa\Enums\NezasaPaymentMethodEnum;
 use Nezasa\Checkout\Integrations\Oppwa\Connectors\OppwaConnector;
+use Nezasa\Checkout\Integrations\Oppwa\Dtos\Payloads\OppwaComplationPayload;
 use Nezasa\Checkout\Integrations\Oppwa\Dtos\Payloads\OppwaPreparePayload;
 use Nezasa\Checkout\Integrations\Oppwa\Dtos\Responses\OppwaPrepareResponse;
 use Nezasa\Checkout\Payments\Contracts\WidgetPaymentContract;
+use Nezasa\Checkout\Payments\Dtos\AbortResult;
 use Nezasa\Checkout\Payments\Dtos\AuthorizationResult;
+use Nezasa\Checkout\Payments\Dtos\CaptureResult;
 use Nezasa\Checkout\Payments\Dtos\PaymentAsset;
 use Nezasa\Checkout\Payments\Dtos\PaymentInit;
-use Nezasa\Checkout\Payments\Dtos\PaymentOutput;
 use Nezasa\Checkout\Payments\Dtos\PaymentPrepareData;
-use Nezasa\Checkout\Payments\Enums\TransactionStatusEnum;
 use Throwable;
 
-class OppwaWidgetGateway
-// implements WidgetPaymentContract
+class OppwaWidgetGateway implements WidgetPaymentContract
 {
+    /**
+     * Returns whether the payment gateway is active.
+     */
     public static function isActive(): bool
     {
         return Config::boolean('checkout.integrations.oppwa.active');
     }
 
+    /**
+     * Returns the name of the payment gateway.
+     *
+     * Important: This name will be used to identify the payment gateway in the checkout process
+     * and it has to be unique, please check the previous gateways' names,
+     */
     public static function name(): string
     {
         return Config::string('checkout.integrations.oppwa.name');
     }
 
+    /**
+     * Prepares the payment initiation process.
+     */
     public function prepare(PaymentPrepareData $data): PaymentInit
     {
         try {
@@ -50,7 +61,7 @@ class OppwaWidgetGateway
                     billingCity: $data->contact->address->city,
                     billingPostcode: $data->contact->address->postalCode,
                     billingCountry: str($data->contact->address->country)->before('-')->toString(),
-                    createRegistration: true
+                    paymentType: 'PA'
                 )
             );
 
@@ -64,6 +75,9 @@ class OppwaWidgetGateway
         return new PaymentInit(isAvailable: false, returnUrl: $data->returnUrl);
     }
 
+    /**
+     * Returns the payload required for creating a transaction in Nezasa.
+     */
     public function makeNezasaTransactionPayload(PaymentPrepareData $data, PaymentInit $paymentInit): NezasaPayload
     {
         if (! $paymentInit->persistentData instanceof OppwaPrepareResponse) {
@@ -78,6 +92,9 @@ class OppwaWidgetGateway
         );
     }
 
+    /**
+     * Returns the assets required for the payment initiation process.
+     */
     public function getAssets(PaymentInit $paymentInit): PaymentAsset
     {
         if (! $paymentInit->persistentData instanceof OppwaPrepareResponse) {
@@ -99,22 +116,27 @@ class OppwaWidgetGateway
 
         return new PaymentAsset(
             isAvailable: true,
-            /** @phpstan-ignore-next-line  */
+            /** @phpstan-ignore-next-line */
             scripts: $scripts->add($script),
             html: $form
         );
     }
 
-    public function verify(Request $request, BaseDto|array $persistentData): AuthorizationResult
+    /**
+     * Handles the callback from the payment gateway to authorize the payment.
+     *
+     * Persistent data is the data that is returned from paymentInit in the prepare method.
+     *
+     * @param  array<string, mixed>  $persistentData
+     */
+    public function authorize(Request $request, array $persistentData): AuthorizationResult
     {
         try {
             $response = OppwaConnector::make()->checkout()->status($request->query('resourcePath'));
-            dd($response->array());
 
             return new AuthorizationResult(
-                //                status: $response->failed() ? TransactionStatusEnum::Failed : TransactionStatusEnum::Succeeded,
-                isSuccessful: true,
-                resultData: (array) $response->array(),
+                isSuccessful: ! $response->failed(),
+                resultData: ['status' => (array) $response->array()]
             );
         } catch (Throwable $exception) {
             report($exception);
@@ -124,10 +146,69 @@ class OppwaWidgetGateway
     }
 
     /**
-     * Show the payment result to the user.
+     * Capture the authorized payment. This method is called after the payment is authorized
+     * and booking itinerary call is successful.
+     *
+     * Persistent data is the data returned from paymentInit in the prepare method.
+     *
+     * @param  array<string, mixed>  $persistentData
+     *
+     * Result data is the data returned from AuthorizationResult's resultData property.
+     * @param  array<string, mixed>  $resultData
      */
-    public function output(AuthorizationResult $result, PaymentOutput $output): PaymentOutput
+    public function capture(Request $request, array $persistentData, array $resultData): CaptureResult
     {
-        return $output;
+        try {
+            $response = OppwaConnector::make()->checkout()->complete(
+                id: $resultData['status']['id'],
+                payload: new OppwaComplationPayload(
+                    amount: $resultData['status']['amount'],
+                    currency: $resultData['status']['currency'],
+                    paymentType: 'CP'
+                )
+            );
+
+            $resultData['capture'] = $response->array();
+
+            return new CaptureResult(isSuccessful: ! $response->failed(), persistentData: $resultData);
+
+        } catch (Throwable $exception) {
+            report($exception);
+
+            return new CaptureResult(isSuccessful: false);
+        }
+    }
+
+    /**
+     * Abort the payment process. This method is called when the booking itinerary call fails.
+     *
+     * Persistent data is the data returned from paymentInit in the prepare method.
+     *
+     * @param  array<string, mixed>  $persistentData
+     *
+     * Result data is the data returned from AuthorizationResult's resultData property.
+     * @param  array<string, mixed>  $resultData
+     */
+    public function abort(Request $request, array $persistentData, array $resultData): AbortResult
+    {
+        try {
+            $response = OppwaConnector::make()->checkout()->complete(
+                id: $resultData['status']['id'],
+                payload: new OppwaComplationPayload(
+                    amount: $resultData['status']['amount'],
+                    currency: $resultData['status']['currency'],
+                    paymentType: 'RV'
+                )
+            );
+
+            $resultData['capture'] = $response->array();
+
+            return new AbortResult(isSuccessful: ! $response->failed(), persistentData: $resultData);
+
+        } catch (Throwable $exception) {
+            report($exception);
+
+            return new AbortResult(isSuccessful: false);
+        }
     }
 }
