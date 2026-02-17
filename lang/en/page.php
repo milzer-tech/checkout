e175b35c13da6c9354d8af400c41a6b54beee40f
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-02-17 12:13:40 +0100

    Add translation for remaining amount payment in English language file

--- lang/en/page.php
+++ lang/en/page.php
@@ -77,6 +77,7 @@ return [
         'conditions_of_payment' => 'Conditions of the payment',
         'rest_payment_will_be_payable_later' => 'The rest of the payment will be payable 14 days before departure. You will receive a link to complete that payment.',
         'you_will_pay_down_payment' => 'You will be paying :percentage of down payment now.',
+        'you_will_pay_remaining' => 'You will be paying the remaining amount now.',
     ],
 
     'payment' => [
