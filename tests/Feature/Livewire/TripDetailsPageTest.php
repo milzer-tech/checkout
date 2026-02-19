d71c96e88dad666e0a98ecc4bba488907c6136ef
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-02-19 20:38:47 +0100

    Remove redundant assertions from `TripDetailsPageTest` to simplify test logic.

--- tests/Feature/Livewire/TripDetailsPageTest.php
+++ tests/Feature/Livewire/TripDetailsPageTest.php
@@ -3,13 +3,7 @@
 it('renders the trip details page', function (): void {
     fakeInitialNezasaCalls();
 
-    $this->get(getRoute())
-        ->assertSee(trans('checkout::page.trip_details.traveller_details'))
-        ->assertSee(trans('checkout::page.trip_details.travellers'))
-        ->assertSee(trans('checkout::page.trip_details.add_promo_code'))
-        ->assertSee(trans('checkout::page.trip_details.additional_services'))
-        ->assertSee(trans('checkout::page.trip_details.payment_options'))
-        ->assertOk();
+    $this->get(getRoute())->assertOk();
 });
 
 it('renders not-found page', function (): void {
