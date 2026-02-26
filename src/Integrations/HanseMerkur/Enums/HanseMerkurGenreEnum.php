8340ca260d789bc973b9223433883ca7a7e65441
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-02-26 11:35:32 +0100

    Add `HanseMerkurGenreEnum` to represent traveler gender options in Hanse Merkur integration

---
+++ src/Integrations/HanseMerkur/Enums/HanseMerkurGenreEnum.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Integrations\HanseMerkur\Enums;
+
+use AzimKordpour\PowerEnum\Traits\PowerEnum;
+
+/**
+ * Enum representing the gender options for traveler information in the HanseMerkur Checkout API.
+ *
+ * @method bool isMale()
+ * @method bool isFemale()
+ * @method bool isChild()
+ * @method bool isCompany()
+ */
+enum HanseMerkurGenreEnum: string
+{
+    use PowerEnum;
+
+    case Male = 'MALE';
+    case Female = 'FEMALE';
+    case Child = 'CHILD';
+    case Company = 'COMPANY';
+}
