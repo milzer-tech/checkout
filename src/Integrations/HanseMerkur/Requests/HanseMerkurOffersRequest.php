286b81a55b4894fea3c5b3781b888ef0987bc49c
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-02-26 11:35:43 +0100

    Add `HanseMerkurOffersRequest` for handling offer requests in Hanse Merkur integration

---
+++ src/Integrations/HanseMerkur/Requests/HanseMerkurOffersRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Integrations\HanseMerkur\Requests;
+
+use Nezasa\Checkout\Integrations\HanseMerkur\Dtos\Payloads\HanseMerkurOffersPayload;
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Traits\Body\HasJsonBody;
+
+class HanseMerkurOffersRequest extends Request implements HasBody
+{
+    use HasJsonBody;
+
+    /**
+     * Define the HTTP method.
+     */
+    protected Method $method = Method::POST;
+
+    /**
+     * Create a new instance of HanseMerkurOffersRequest
+     */
+    public function __construct(public HanseMerkurOffersPayload $payload) {}
+
+    /**
+     * Define the endpoint for the request.
+     */
+    public function resolveEndpoint(): string
+    {
+        return 'v1/offers';
+    }
+
+    /**
+     * Define the body of the request.
+     *
+     * @return array<string, mixed>
+     */
+    protected function defaultBody(): array
+    {
+        return $this->payload->toArray();
+    }
+}
