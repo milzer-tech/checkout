9b03efd417349dd546e1182ae29e1b1a05eb2d0a
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-03-11 15:14:40 +0100

    Add `GetActiveInsuranceAction` for retrieving the active insurance provider
    - Introduced a new action to identify and validate the active insurance provider.
    - Ensures only one provider can be active at a time and implements validation via `InsuranceContract`.

---
+++ src/Actions/Insurance/GetActiveInsuranceAction.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Actions\Insurance;
+
+use Illuminate\Support\Facades\Config;
+use InvalidArgumentException;
+use Nezasa\Checkout\Insurances\Contracts\InsuranceContract;
+
+final class GetActiveInsuranceAction
+{
+    /**
+     * Check if there is an active insurance provider.
+     */
+    public function run(): ?InsuranceContract
+    {
+        $active = [];
+
+        foreach (Config::get('checkout.insurance_provider') as $insurance) {
+            if (! in_array(InsuranceContract::class, class_implements($insurance))) {
+                throw new InvalidArgumentException(
+                    "the insurance $insurance is not an instance of InsuranceContract"
+                );
+            }
+
+            if ($insurance::isActive()) {
+                $active[] = new $insurance;
+            }
+        }
+
+        if (count($active) > 1) {
+            throw new InvalidArgumentException('Only one insurance provider can be active at a time.');
+        }
+
+        return $active[0] ?? null;
+    }
+}
