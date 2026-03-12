181e72651a643f048016db8a1bdf80712f6cee4e
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-03-12 18:40:14 +0100

    Add `bookOffer` method to `InsuranceContract` interface to support insurance booking

--- src/Insurances/Contracts/InsuranceContract.php
+++ src/Insurances/Contracts/InsuranceContract.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Nezasa\Checkout\Insurances\Contracts;
 
+use Nezasa\Checkout\Insurances\Dtos\BookInsuranceOfferDto;
 use Nezasa\Checkout\Insurances\Dtos\CreateInsuranceOffersDto;
 use Nezasa\Checkout\Insurances\Dtos\InsuranceOffersResult;
 
@@ -12,4 +13,6 @@ interface InsuranceContract
     public static function isActive(): bool;
 
     public function getOffers(CreateInsuranceOffersDto $createOffersDto): InsuranceOffersResult;
+
+    public function bookOffer(BookInsuranceOfferDto $bookOfferDto);
 }
