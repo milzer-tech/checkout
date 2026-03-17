8df766c1e457d10b66200490cca9e48edc196fc7
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-03-17 12:17:00 +0100

    Add new methods to InsuranceContract for enhanced functionality
    - Introduce `isActive` and `getName` methods for insurance status and provider name handling.
    - Update `bookOffer` to return `InsuranceBookOfferResult`.
    - Add `getNezasaPayload` for custom payload

--- src/Insurances/Contracts/InsuranceContract.php
+++ src/Insurances/Contracts/InsuranceContract.php
@@ -6,13 +6,35 @@ namespace Nezasa\Checkout\Insurances\Contracts;
 
 use Nezasa\Checkout\Insurances\Dtos\BookInsuranceOfferDto;
 use Nezasa\Checkout\Insurances\Dtos\CreateInsuranceOffersDto;
+use Nezasa\Checkout\Insurances\Dtos\InsuranceBookOfferResult;
 use Nezasa\Checkout\Insurances\Dtos\InsuranceOffersResult;
+use Nezasa\Checkout\Integrations\Nezasa\Dtos\Payloads\AddCustomInsurancePayload;
 
 interface InsuranceContract
 {
+    /**
+     * Indicates if the insurance is active.
+     */
     public static function isActive(): bool;
 
+    /**
+     * The name of the insurance provider.
+     * We use this name to populate the Nezasa payload or display it in the checkout process.
+     */
+    public static function getName(): string;
+
+    /**
+     * Creates insurance offers.
+     */
     public function getOffers(CreateInsuranceOffersDto $createOffersDto): InsuranceOffersResult;
 
-    public function bookOffer(BookInsuranceOfferDto $bookOfferDto);
+    /**
+     * Book an insurance offer.
+     */
+    public function bookOffer(BookInsuranceOfferDto $bookOfferDto): InsuranceBookOfferResult;
+
+    /**
+     * In case if you need to customize data sending to Nezasa, you can do it here.
+     */
+    public function getNezasaPayload(AddCustomInsurancePayload $payload): AddCustomInsurancePayload;
 }
