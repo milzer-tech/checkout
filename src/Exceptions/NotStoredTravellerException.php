867b4373519c10cc420757ec21618ab7c1344f68
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-03-23 15:46:54 +0100

    Add `NotStoredTravellerException` to handle specific traveller storage errors

---
+++ src/Exceptions/NotStoredTravellerException.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Exceptions;
+
+use Illuminate\Http\Response as LaravelResponse;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class NotStoredTravellerException extends HttpException
+{
+    /**
+     * Create a new instance of the ParentException
+     */
+    public function __construct(array $travellers = [])
+    {
+        parent::__construct(
+            statusCode: SymfonyResponse::HTTP_CONFLICT,
+            message: 'Traveller not stored, response received from Nezasa:'.json_encode($travellers),
+        );
+    }
+
+    /**
+     * Render the exception as an HTTP response.
+     */
+    public function render(): LaravelResponse
+    {
+        return new LaravelResponse(
+            /** @phpstan-ignore-next-line */
+            content: view(view: 'checkout::exceptions.all')->with('exception', $this),
+            status: SymfonyResponse::HTTP_NOT_FOUND
+        );
+    }
+}
