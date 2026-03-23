2d9aeaeec227a4e0d646f709f0ff14f19ba0f3f1
Mojtaba <azim.kordpour.hendekhaleh@gmail.com>
2026-03-23 15:47:11 +0100

    Add `SaveTravellersOnNezasaAction` to manage traveller data synchronization with Nezasa API

---
+++ src/Actions/Checkout/SaveTravellersOnNezasaAction.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nezasa\Checkout\Actions\Checkout;
+
+use Illuminate\Support\Collection;
+use Nezasa\Checkout\Exceptions\NotStoredTravellerException;
+use Nezasa\Checkout\Integrations\Nezasa\Connectors\NezasaConnector;
+use Nezasa\Checkout\Integrations\Nezasa\Dtos\Payloads\Entities\ContactInfoPayloadEntity;
+use Nezasa\Checkout\Integrations\Nezasa\Dtos\Payloads\Entities\PaxInfoPayloadEntity;
+use Nezasa\Checkout\Integrations\Nezasa\Dtos\Payloads\SaveTravellersDetailsPayload;
+use Nezasa\Checkout\Models\Checkout;
+
+final class SaveTravellersOnNezasaAction
+{
+    public function run(Checkout $model)
+    {
+        /** @var Collection<int, PaxInfoPayloadEntity> $paxInfo */
+        $paxInfo = new Collection;
+
+        try {
+            /** @phpstan-ignore-next-line */
+            foreach (collect($model->data['paxInfo'] ?? [])->flatten(1) as $index => $pax) {
+                $paxInfo[] = PaxInfoPayloadEntity::from([
+                    'refId' => "pax-$index",
+                    ...$pax,
+                ]);
+            }
+
+            $payload = new SaveTravellersDetailsPayload(
+                contactInfo: ContactInfoPayloadEntity::from($model->data['contact']),
+                paxInfo: $paxInfo
+            );
+
+            $response = resolve(NezasaConnector::class)
+                ->checkout()
+                ->saveTravelerDetails($model->checkout_id, $payload);
+
+            if ($response->failed()) {
+                $model->updateData([
+                    'travelerDetailsError' => $response->array(),
+                ]);
+
+                throw new NotStoredTravellerException($response->array());
+            }
+        } catch (\Throwable $e) {
+            throw new NotStoredTravellerException([$e->getMessage()]);
+        }
+    }
+}
