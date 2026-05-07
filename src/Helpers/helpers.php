a4d23523b44f2f0aefef42eb646dbf3195c6af5b
Azim Kordpour <mojtaba.kordpour@gmail.com>
2026-05-07 10:39:46 +0200

    MLZ-4796: Add helper functions for trip builder and component replacement URLs

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Nezasa\Checkout\Dtos\Contracts\NezasaComponentDtoContract;
+
 if (! function_exists('checkout_path')) {
     /**
      * Get the root path of the package.
@@ -15,5 +17,34 @@ if (! function_exists('checkout_path')) {
         return $path === ''
             ? $root
             : $root.DIRECTORY_SEPARATOR.$path;
+    }
+}
+
+if (! function_exists('getUrlToTripBuilder')) {
+    /**
+     * Get the url to the trip builder.
+     */
+    function getUrlToTripBuilder(): string
+    {
+        $baseUrl = config('checkout.nezasa.base_url');
+        $itineraryId = request()->query('itineraryId');
+
+        return request()->query('goto') === 'smartplanner'
+            ? $baseUrl.'/itinerary-apps/smartplanner/'.$itineraryId.'?goto=smartplanner'
+            : $baseUrl.'/itineraries/'.$itineraryId;
+    }
+}
+
+if (! function_exists('getUrlToReplaceComponent')) {
+    /**
+     * Get the url to replace the component.
+     */
+    function getUrlToReplaceComponent(NezasaComponentDtoContract $componentDto): string
+    {
+        $type = $componentDto->getType()->isTransport() ? 'flight' : $componentDto->getType()->toLower();
+
+        return request()->query('goto') === 'smartplanner'
+            ? getUrlToTripBuilder().'&openDrawer='.$type.'&componentId='.$componentDto->getId()
+            : getUrlToTripBuilder();
     }
 }
