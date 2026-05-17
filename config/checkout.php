54c9dc300347e79f25c12f3c880f2a4a4467e1e4
Azim Kordpour <mojtaba.kordpour@gmail.com>
2026-05-17 20:07:40 +0200

    MLZ-4864: Update `getUrlToTripBuilder` to support IBE-specific base URL configuration

--- config/checkout.php
+++ config/checkout.php
@@ -17,6 +17,7 @@ return [
      * All the configuration options for the Nezasa API is defined here.
      */
     'nezasa' => [
+        'ibe_base_url' => env('CHECKOUT_NEZASA_IBE_BASE_URL', 'https://milzercheckout-ibe.stg.tripbuilder.app'),
         'base_url' => env('CHECKOUT_NEZASA_BASE_URL', 'https://api.tripbuilder.app'),
         'username' => env('CHECKOUT_NEZASA_USERNAME', 'must_be_set_in_env'),
         'password' => env('CHECKOUT_NEZASA_PASSWORD', 'must_be_set_in_env'),
